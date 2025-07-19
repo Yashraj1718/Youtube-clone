9c3dff03b574085b9048b0551b360992bd85ed06
RedBunny1718 <iamblackdevil1718@gmail.com>
2025-07-19 20:08:59 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+const  asyncHandler = (requestHandler) => (req,res,next) =>{
+    Promise.resolve(requestHandler(req,res,next)).catch((err) =>{
+        next(err)
+    })
+}
+
+export {asyncHandler}
+
+
+//const asyncHandler = () => {}
+//const asyncHandler = (function) => {}
+//const asyncHandler = (function) => 
+// () => {}
+//                                    
+//
+//this above thing is called higherorder function which taked function as paramerter so basically thing functtion act as a variable here
+
+
+
+
+// this is how u can also write it but 1st is hard but you should both types to write it
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+        
+//     }
+// }
