06438f63c76d7d31c536e6b53b1d898bd3c2c573
RedBunny1718 <iamblackdevil1718@gmail.com>
2025-07-21 23:32:30 +0530

    added controller code and its logic

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,8 +16,11 @@ const uploadCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         //file has been uploaded successfully
-        console.log("file has been uploaded on cloudinary",response.url);
-        return response;
+        // console.log("file has been uploaded on cloudinary",response.url);
+        // return response;
+
+        fs.unlinkSync(localFilePath)
+        return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
